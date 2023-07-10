d30391df2582e00813582d52b883c2f0afbd8a96
Murodov Mirshod Sherzodovich <mirshodmurodov67@gmail.com>
2023-07-10 07:34:08 +0000

    fix:Update RegOneId.jsx

--- src/pages/Reg/RegOneId.jsx
+++ src/pages/Reg/RegOneId.jsx
@@ -18,7 +18,7 @@ const RegOneId = () => {
         <a
           target="_blank"
           onClick={() =>
-            (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/&state=test&scope=myportal`)
+            (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/oneid/one&state=test&scope=myportal`)
           }
         >
           {/*  process.env.NODE_ENV === "production"
