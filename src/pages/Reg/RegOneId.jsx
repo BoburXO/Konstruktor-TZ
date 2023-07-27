2bcacac3a96940b4a34ee326ec3a607d130e81a7
Bobur Hatamov <boburhatamov7@gmail.com>
2023-07-27 10:52:30 +0500

    fix:login page changed

--- src/pages/Reg/RegOneId.jsx
+++ src/pages/Reg/RegOneId.jsx
@@ -2,16 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import s from "../Reg/regOneId.module.css";
 import oneId from "../../assets/icons/one-login.svg";
-import Select from "react-select";
-import { useTranslation } from "react-i18next";
 
 const RegOneId = () => {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-    window.location.reload();
-  };
   const navigate = useNavigate();
   React.useEffect(() => {
     if (localStorage.getItem("ConstructorRoleAccessToken")) {
@@ -19,52 +11,21 @@ const RegOneId = () => {
     }
   }, []);
 
-  const options = [
-    { label: "Русский", value: "ru" },
-    { label: "O'zbek", value: "uz" },
-    { label: "Ўзбек", value: "kr" },
-  ];
-
   return (
-    <div style={{ padding: "40px" }}>
-      <Select
-        className={s.select_nav}
-        options={options}
-        placeholder={
-          localStorage.getItem("i18nextLng") === "ru"
-            ? "Русский"
-            : localStorage.getItem("i18nextLng") === "uz"
-            ? "O'zbek"
-            : "Ўзбек"
-        }
-        onChange={({ value }) => changeLanguage(value)}
-        isSearchable={false}
-      />
-      <div className={s.reg_parent}>
-        <div className={s.reg_card}>
-          <h1>{t("reg.1")}</h1>
-          <a
-            target="_blank"
-            onClick={() =>
-              (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/oneid/one&state=test&scope=myportal`)
-            }
-          >
-            {/*  process.env.NODE_ENV === "production"
-                  ? "https://constructor.egov.uz"
-                  : "http://localhost:3000"*/}
-            {localStorage.getItem("i18nextLng") === "ru" ? (
-              <button style={{ paddingLeft: "20px" }}>
-                {t("reg.2")}
-                <img src={oneId} alt="ONE ID" />
-              </button>
-            ) : (
-              <button>
-                <img src={oneId} alt="ONE ID" />
-                {t("reg.2")}
-              </button>
-            )}
-          </a>
-        </div>
+    <div className={s.reg_parent}>
+      <div className={s.reg_card}>
+        <h1>Вход в систему</h1>
+        <a
+          target="_blank"
+          onClick={() =>
+            (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/oneid/one&state=test&scope=myportal`)
+          }
+        >
+          <button>
+            Войти через
+            <img src={oneId} alt="ONE ID" />
+          </button>
+        </a>
       </div>
     </div>
   );
--- src/pages/oneid/RegOneId.jsx
+++ src/pages/oneid/RegOneId.jsx
@@ -1,63 +1,50 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import Loader from "../../components/Loader/Loader";
 import { Context } from "../../Context/Context";
 import s from "../Reg/regOneId.module.css";
-import oneId from "../../assets/icons/one-login.svg";
-import { useTranslation } from "react-i18next";
 
 const OneId = () => {
-  const { t } = useTranslation();
-
-  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+  React.useEffect(() => {
+    if (localStorage.getItem("ConstructorRoleAccessToken")) {
+      navigate("/main");
+    }
+  }, []);
+
   const search = useLocation().search;
   const code = new URLSearchParams(search).get("code");
   const { ssoOneId, roles, RoleId } = useContext(Context);
 
   useEffect(() => {
-    if (localStorage.getItem("ConstructorRoleAccessToken")) {
-      navigate("/main");
-    }
     localStorage.setItem("oneIDCode", code);
-    ssoOneId().then(() => setIsLoading(false));
+    ssoOneId();
   }, []);
 
-  if (isLoading) return <Loader />;
-
   return (
-    <div>
-      <div className={s.reg_parent}>
-        {localStorage.getItem("oneIDcCode") ? (
-          <div className={s.reg_card}>
-            <h1>{t("reg.1")}</h1>
-            <a
-              target="_blank"
-              onClick={() =>
-                (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/oneid/one&state=test&scope=myportal`)
-              }
-            >
-              {/*  process.env.NODE_ENV === "production"
-                  ? "https://constructor.egov.uz"
-                  : "http://localhost:3000"*/}
-              <button>
-              {t("reg.2")}
-              <img src={oneId} alt="ONE ID" />
-            </button>
-            </a>
-          </div>
-        ) : (
-          <div className={s.reg_user_roles}>
-            {roles?.roles?.map((role) => {
-              return (
-                <button onClick={() => RoleId(role?.id)} key={role?.id}>
-                  {role?.name}
-                </button>
-              );
-            })}
-          </div>
-        )}
-      </div>
+    <div className={s.reg_parent}>
+      {localStorage.getItem("oneIDcCode") ? (
+        <div className={s.reg_card}>
+          <h1>Вход в систему</h1>
+          <a
+            target="_blank"
+            onClick={() =>
+              (window.location.href = `https://sso.egov.uz/sso/oauth/Authorization.do?response_type=one_code&client_id=single_reester&redirect_uri=https://constructor.egov.uz/&state=test&scope=myportal`)
+            }
+          >
+            <button>Войти через OneID</button>
+          </a>
+        </div>
+      ) : (
+        <div className={s.reg_user_roles}>
+          {roles?.roles?.map((role) => {
+            return (
+              <button onClick={() => RoleId(role?.id)} key={role?.id}>
+                {role?.name}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
