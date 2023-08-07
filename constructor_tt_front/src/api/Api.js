6205d7156bf17b53273cd327c62b0d57a63f26f8
Bobur Hatamov <boburhatamov7@gmail.com>
2023-08-07 15:07:10 +0500

    fix: git updated

--- constructor_tt_front/src/api/Api.js
+++
@@ -1,6 +0,0 @@
-export const API = `https://constructor.egov.uz${
-  localStorage.getItem("i18nextLng") === "uz" ||
-  localStorage.getItem("i18nextLng") === "kr"
-    ? ""  
-    : "/ru"
-}/api/v1`;
--- constructor_tt_front/src/components/Loader/Loader.js
+++
@@ -1,5 +0,0 @@
-import Loading from "react-fullscreen-loading";
-
-export default function Loader() {
-  return <Loading loading background="#fff" loaderColor="#3498db" />;
-}
